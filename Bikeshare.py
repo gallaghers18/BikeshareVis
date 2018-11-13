1ba0fded23276526c5918d8fc679ed7016dbc3c6
gallaghers18 <gallaghers@carleton.edu>
2018-11-12 22:08:44 -0600

    Added them all

---
+++ Bikeshare.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify
+import random
+app = Flask(__name__)
+
+@app.route('/')
+def forcePlot():
+    return render_template('Bikeshare.html')
+
+@app.route('/stationData/')
+def getStationData():
+    with open('Capital_Bike_Share_Locations.csv') as csvData:
+        data = []
+        next(csvData)
+        for line in csvData:
+            listy = line.strip().split(',')
+            entry = {}
+            entry['Id'] = listy[1]
+            entry['Address'] = listy[2]
+            entry['Latitude'] = listy[4]
+            entry['Longitude'] = listy[5]
+            data.append(entry)
+    return jsonify({'data': data})
+
+#HERE WE WILL DO ALL OUR AVERAGING AND ANY OTHER PREPROCESSING, BUT FOR NOW I HAVEN'T DONE THAT
+@app.route('/Q1Data/')
+def getQuarterOne():
+    with open('2017Q1-capitalbikeshare-tripdata.csv') as csvData:
+        data = []
+        next(csvData)
+        for line in csvData:
+            listy = line.strip().split(',')
+            entry = {}
+            entry['DateOut'] = listy[1]
+            entry['DateIn'] = listy[2]
+            entry['AddressOut'] = listy[4]
+            entry['AddressIn'] = listy[6]
+            entry['MemberType'] = listy[8]
+            data.append(entry)
+    return jsonify({'data': data})
+
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ static/Bikeshare.js
@@ -0,0 +1,63 @@
+/*
+ * Bikeshare Viz
+ */
+
+ //Size parameters
+var width = 500;
+var height = 500;
+var stationData;
+var Q1Data;
+var Q2Data;
+var Q3Data;
+var Q4Data;
+
+var getStationData = '/stationData/';
+var getQ1Data = '/Q1Data/';
+var getQ2Data = '/Q2Data/';
+var getQ3Data = '/Q3Data/';
+var getQ4Data = '/Q4Data/';
+
+
+// Add svg and g elements to the webpage
+var svgMap = d3.select("#mapDiv").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+ 
+
+var drawStations = function() {
+    xScale = d3.scale.linear()
+                .domain([d3.min(stationData, function(d) { return parseFloat(d['Latitude']); }),
+                        d3.max(stationData, function(d) { return parseFloat(d['Latitude']); })])
+                .range([0, width]);
+    
+    yScale = d3.scale.linear()
+                .domain([d3.min(stationData, function(d) { return parseFloat(d['Longitude']); }),
+                        d3.max(stationData, function(d) { return parseFloat(d['Longitude']); })])
+                .range([height, 0]);
+                
+    station = svgMap.selectAll('.station')
+        .data(stationData);
+
+    station
+        .enter()
+        .append('svg:circle')
+        .attr('class', 'station')
+        .attr('r', 2)
+        .attr('cx', function(d) {return xScale(d['Latitude'])})
+        .attr('cy', function(d) {return yScale(d['Longitude'])});
+}
+
+
+
+d3.json(getStationData, function(error, data) {
+    stationData = data['data'];
+    // RIGHT NOW LOADING THE BIG DATASETS IS BEING REALLY SLOW SO 
+    //WE WILL NEED TO FIGURE OUT HOW WE ARE SUPPOSED TO DO IT.
+    // d3.json(getQ1Data, function(error, data) {
+        // console.log(data)
+        drawStations();
+    // });
+  });
+
+ 
