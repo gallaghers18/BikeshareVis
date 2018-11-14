eed0affb9fb91bc6f9712ad2d91ed0663591170b
Alejandro G <gallardoa@carelton.edu>
2018-11-13 23:56:40 -0600

    Edits

--- formatting.py
+++ formatting.py
@@ -1,6 +1,7 @@
 import datetime
 
 Q1dataIn = {}
+dummy = {}
 with open('2017Q1-capitalbikeshare-tripdata.csv') as csvData:
     next(csvData)
 
@@ -9,17 +10,52 @@ with open('2017Q1-capitalbikeshare-tripdata.csv') as csvData:
     for line in csvData:
         listy = line.replace('"', '').strip().split(',')
         addressIn = listy[6]
-        print(type(addressIn))
-        if addressIn not in Q1dataIn:
-            Q1dataIn[addressIn : ]
         
 
         temp = listy[2].split()
         dateIn = temp[0].split('-')    # 0 -> year, 1 -> month, 2 -> day
-        dayIn = datetime.date(int(dateIn[0]), int(dateIn[1]), int(dateIn[2])).weekday()
+        timeIn = temp[1].split(':')    # 0 -> hour, 1 -> minute, 2 -> seconds
+        dayIn = dayMap[datetime.date(int(dateIn[0]), int(dateIn[1]), int(dateIn[2])).weekday()]
 
-        Q1dataIn[addressIn]['dayIn' : dayIn]
-        print(Q1dataIn[addressIn]['dayIn'])
+        timeBin = int(timeIn[0]) #determine what time bin current station falls into
+        if int(timeIn[1])>30:
+            timeBin = timeBin + .5 # 0 means 12:00am, 0.5 means 12:30am, 1 means 1am
+        
+        if addressIn not in Q1dataIn:
+            Q1dataIn[addressIn] = {} #make addressIn a key and set value as nested dic
+            Q1dataIn[addressIn][dayIn] = {} #make dayIn a key in nested dic set value as nested^2 dic
+            Q1dataIn[addressIn][dayIn][timeBin] = 1 #make timeBin a key in nested^2 dic
+        else:
+            if dayIn not in Q1dataIn[addressIn]:
+                Q1dataIn[addressIn][dayIn] =  {}
+                Q1dataIn[addressIn][dayIn][timeBin] = 1
+            else:
+                if timeBin not in Q1dataIn[addressIn][dayIn]:
+                    Q1dataIn[addressIn][dayIn][timeBin] = 1
+                else:
+                    Q1dataIn[addressIn][dayIn][timeBin]+=1
+        if addressIn == '7th & E St SW':
+            print(addressIn, dayIn, timeBin)
+
+    print(Q1dataIn['7th & E St SW'])
+
+        
+
+        
+            # print("inner",Q1dataIn[addressIn])
+        # print(Q1dataIn[addressIn], dayIn, dayMap[dayIn])
+            
+        # print(dayIn, Q1dataIn[addressIn], dayMap[dayIn]) 
+        # print(Q1dataIn)
+        # else:
+        #     Q1dataIn[addressIn[dayIn]]
+
+
+        
+        # print(temp)
+
+        # Q1dataIn[addressIn]['dayIn' : dayIn]
+        # print(Q1dataIn[addressIn]['dayIn'])
 
 
 
